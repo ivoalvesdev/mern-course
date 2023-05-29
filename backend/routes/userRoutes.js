24949424da287c3ea8196c99acc15291cbf61470
Ivo Alves <ivo420alves@gmail.com>
2023-05-29 22:16:41 +0100

    Connect to User Routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,9 @@ import {
 
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
