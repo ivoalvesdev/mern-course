511bdec5867acf49fb7ac0797790d01e25d4726a
Ivo Alves <ivo420alves@gmail.com>
2023-05-29 22:04:37 +0100

    Custom Error Middleware

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => res.send("serevr is ready"));
 
